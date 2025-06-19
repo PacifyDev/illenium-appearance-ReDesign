d5b7693a52414959123a9991922fbad84fa97380
PacifyDev <iveraab4@gmail.com>
2025-06-19 12:34:12 +0200

    Add files via upload

---
+++ illenium-appearance/shared/theme.lua
@@ -0,0 +1,61 @@
+Config.Theme = {
+    currentTheme = "qb-core",
+    themes = {
+        {
+            id = "default",
+            borderRadius = "4px",
+            fontColor = "255, 255, 255",
+            fontColorHover = "0, 173, 238",
+            fontColorSelected = "34, 40, 49",
+            fontFamily = "Inter",
+            primaryBackground = "34, 40, 49",
+            primaryBackgroundSelected = "0, 173, 238",
+            secondaryBackground = "24, 28, 36",
+            scaleOnHover = false,
+            sectionFontWeight = "normal",
+            smoothBackgroundTransition = false
+        },
+        {
+            id = "qb-core",
+            borderRadius = "3vh",
+            fontColor = "255, 255, 255",
+            fontColorHover = "0, 173, 238",
+            fontColorSelected = "34, 40, 49",
+            fontFamily = "Poppins",
+            primaryBackground = "34, 40, 49",
+            primaryBackgroundSelected = "0, 173, 238",
+            secondaryBackground = "24, 28, 36",
+            scaleOnHover = true,
+            sectionFontWeight = "bold",
+            smoothBackgroundTransition = true
+        },
+        {
+            id = "project-sloth",
+            borderRadius = "6vh",
+            fontColor = "255, 255, 255",
+            fontColorHover = "0, 173, 238",
+            fontColorSelected = "34, 40, 49",
+            fontFamily = "Inter",
+            primaryBackground = "34, 40, 49",
+            primaryBackgroundSelected = "0, 173, 238",
+            secondaryBackground = "24, 28, 36",
+            scaleOnHover = true,
+            sectionFontWeight = "bold",
+            smoothBackgroundTransition = false
+        },
+        {
+            id = "not-heavily-inspired",
+            borderRadius = "10vh",
+            fontColor = "255, 255, 255",
+            fontColorHover = "0, 173, 238",
+            fontColorSelected = "34, 40, 49",
+            fontFamily = "Inter",
+            primaryBackground = "34, 40, 49",
+            primaryBackgroundSelected = "0, 173, 238",
+            secondaryBackground = "24, 28, 36",
+            scaleOnHover = true,
+            sectionFontWeight = "bold",
+            smoothBackgroundTransition = false
+        }
+    }
+}
